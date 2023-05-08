db8b458928e75a3d135a9f048f8eb4d016ceffb8
Ahmad habib <ahmad.habib@whizzbridge.com>
2023-05-08 17:04:52 +0500

    Steps completed

--- src/modules/Onboarding/PhilosophyGoals/index.tsx
+++ src/modules/Onboarding/PhilosophyGoals/index.tsx
@@ -1,19 +1,20 @@
+import { useLayoutEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Header from "../../../shared/components/Header";
 import { useNavigate, useParams } from "react-router-dom";
 import { RootState } from "../../../redux-toolkit/store/store";
-import { useLayoutEffect, useState } from "react";
 import Objective from "./Objective";
 import Requirement from "./Requirement";
-import Drawer from "../../../shared/components/Drawer";
 import Modal from "../../../shared/components/Modal";
+import InvestmentCriteria from "./InvestmentCriteria";
+import InvestmentHorizon from "./InvestmentHorizon";
+import Loss from "./Loss";
 
 const PhilosophyGoals = (props: any) => {
     const params = useParams();
     const navigate = useNavigate();
     const language: any = useSelector((state: RootState) => state.language.value);
     const [selection, setSelection] = useState(Number(params?.id) || 1);
-    const [isOpen, setOpen] = useState(false);
     const [modalOpen, setModalOpen] = useState(false);
 
     useLayoutEffect(() => {
@@ -25,15 +26,13 @@ const PhilosophyGoals = (props: any) => {
             <section className="h-[67px]">
                 <Header custom={true} data={{ leftMenu: language.individual.philosophyGoals, button: <button className="text-neutral-900 bg-white font-bold text-sm w-[150px] h-9 border border-black shadow-sm">{language.buttons.gotoDashboard}</button> }} />
             </section>
-            <Drawer isOpen={isOpen} setIsOpen={(val: boolean) => setOpen(val)}>
-                <header className="font-bold text-xl">{language.philosophyGoals.objective}</header>
-                <p className="text-neutral-700 font-normal text-sm text-justify">
-                    Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus,
-                    ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl.
-                </p>
-            </Drawer>
-            {selection === 1 && <Objective learnMore={() => setOpen(true)} />}
-            {selection === 2 && <Requirement learnMore={() => setOpen(true)} nextStep={() => setModalOpen(true)} />}
+
+            {selection === 1 && <Objective />}
+            {selection === 2 && <Requirement />}
+            {selection === 3 && <InvestmentCriteria />}
+            {selection === 4 && <InvestmentHorizon />}
+            {selection === 5 && <Loss nextStep={() => setModalOpen(true)} />}
+
             <Modal show={modalOpen}>
                 <div className="p-12 rounded-md shadow-cs-1 flex flex-col items-center w-full bg-white outline-none focus:outline-none">
                     <h3 className="text-xl font-bold">{language.modal.thankyou}</h3>
