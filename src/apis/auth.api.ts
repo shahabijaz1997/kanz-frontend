c3e89e9f4e572fce2cb41e0dd876625a39c80f5f
Ahmad habib <ahmad.habib@whizzbridge.com>
2023-05-12 12:24:24 +0500

    WOrkflow for redirection

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,18 +1,18 @@
-import axiosInstance from './request-parser';
+import axios from "axios";
 import { getEnv } from "../env";
 
 const ENV = getEnv();
 
 export const signin = (payload: any) => {
-    return axiosInstance.post(`${ENV.API_URL}/login`, payload);
+    return axios.post(`${ENV.API_URL}/login`, payload);
 };
 
 export const signup = (payload: any) => {
-    return axiosInstance.post(`${ENV.API_URL}/signup`, payload);
+    return axios.post(`${ENV.API_URL}/signup`, payload);
 };
 
 export const logout = (token: string) => {
-    return axiosInstance.delete(`${ENV.API_URL}/logout`, {
+    return axios.delete(`${ENV.API_URL}/logout`, {
         headers: {
             Authorization: `Bearer ${token}`
         },
@@ -20,11 +20,11 @@ export const logout = (token: string) => {
 };
 
 export const confirmToken = (payload: any) => {
-    return axiosInstance.get(`${ENV.API_URL}/confirmation`, { params: payload });
+    return axios.get(`${ENV.API_URL}/confirmation`, { params: payload });
 };
 
 export const selectInvestorType = (payload: any, token: string) => {
-    return axiosInstance.post(`${ENV.API_URL}/investor/type`, payload, {
+    return axios.post(`${ENV.API_URL}/investor/type`, payload, {
         headers: {
             Authorization: `Bearer ${token}`
         },
@@ -32,7 +32,7 @@ export const selectInvestorType = (payload: any, token: string) => {
 };
 
 export const investmentAccridiation = (payload: any, token: string) => {
-    return axiosInstance.post(`${ENV.API_URL}/investor/accreditation`, payload, {
+    return axios.post(`${ENV.API_URL}/investor/accreditation`, payload, {
         headers: {
             Authorization: `Bearer ${token}`
         },
@@ -41,7 +41,7 @@ export const investmentAccridiation = (payload: any, token: string) => {
 
 
 export const getInvestor = ( token: string) => {
-    return axiosInstance.get(`${ENV.API_URL}/investor`,{
+    return axios.get(`${ENV.API_URL}/investor`,{
         headers: {
             Authorization: `Bearer ${token}`
         },
