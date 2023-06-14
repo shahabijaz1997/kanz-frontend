c1699c5122c8a6c727afc727063bb1c68f7ca376
ahmadwb <ahmad.habib@whizzbridge.com>
2023-06-14 19:34:45 +0500

    Fixes in home screen rtl

--- src/modules/Onboarding/components/ClippedBanner/index.tsx
+++ src/modules/Onboarding/components/ClippedBanner/index.tsx
@@ -17,11 +17,11 @@ const ClippedBanner = () => {
     return (
         <section style={{ clipPath }} className="h-full w-[45%] inline-block align-top relative screen991:hidden">
             <img src={LoginBg} alt={language?.onboarding?.loginBgAlt} className="w-full h-full absolute object-cover" />
-            <aside className="bg-white rounded-[20px] w-[65%] h-[250px] absolute left-1/2 translate-x-[-50%] py-[20px] px-[22px] top-[20vh]">
+            <aside className="bg-white rounded-[20px] w-[65%] h-[250px] absolute left-1/2 translate-x-[-50%] py-[20px] px-[22px] top-[20vh] screen1200:px-[15px]">
                 <img src={QuotesSvg} alt={language?.onboarding?.quotes} />
                 <div className="relative top-[-20px] px-[20px]">
-                    <h2 className="m-0 p-0 text-[36px] font-bold leading-none screen1200:text-[30px]">{language?.onboarding?.welcome}</h2>
-                    <p className="font-normal pt-[30px] text-[16px] leading-7 tracking-[3%] text-neutral-500 screen1200:text-[13px] screen1200:leading-2">{language?.onboarding?.syndicateLeadSub}</p>
+                    <h2 className="m-0 p-0 text-[36px] font-bold leading-none screen1200:text-[30px] screen1200:text-[22px]">{language?.onboarding?.welcome}</h2>
+                    <p className="font-normal pt-[30px] text-[16px] leading-7 tracking-[3%] text-neutral-500 screen1200:text-[13px] screen1200:leading-4 screen1200:leading-2">{language?.onboarding?.syndicateLeadSub}</p>
                 </div>
             </aside>
         </section>
