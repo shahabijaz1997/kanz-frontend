0f497d5ab0351262859cee3566e34a0eccba8a26
ahmadwb <ahmad.habib@whizzbridge.com>
2023-06-22 19:00:19 +0500

    Issues fixed for language

--- src/modules/Onboarding/components/Individual/index.tsx
+++ src/modules/Onboarding/components/Individual/index.tsx
@@ -249,16 +249,7 @@ const Individual = ({ language }: any) => {
         <header className="font-bold text-xl">
           {language.philosophyGoals.whyToDo}
         </header>
-        <p className="text-neutral-700 font-normal text-sm text-justify">
-          Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
-          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
-          elit sed risus. Maecenas eget condimentum velit, sit amet feugiat
-          lectus. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Praesent auctor purus luctus enim
-          egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex.
-          Suspendisse ac rhoncus nisl.
-        </p>
+        <p className="text-neutral-700 font-normal text-sm text-justify">{language?.drawer?.individual}</p>
       </Drawer>
     </form>
   );
