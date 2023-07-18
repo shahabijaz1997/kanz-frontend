5e9d0fa9bc0fd2c2f3b2f66ff59ebc556b916976
shahid <shahidhumeed@gmail.com>
2023-07-18 12:19:04 +0500

    add logo url

--- src/modules/Onboarding/StartupFlow/StartupStepper/index.tsx
+++ src/modules/Onboarding/StartupFlow/StartupStepper/index.tsx
@@ -339,10 +339,10 @@ const StartupStepper = ({ load, event, countries, orientation, language, file, p
                     {payload.logo && typeof payload.logo === "string" ? (
                       <img
                         alt="Logo"
-                        // src={payload.logo}
-                        src={
-                          "https://images.unsplash.com/photo-1686668108582-5c7074494753?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
-                        }
+                        src={payload.logo}
+                        // src={
+                        //   "https://images.unsplash.com/photo-1686668108582-5c7074494753?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+                        // }
                         className="block w-[110%] h-[110%] overflow-hidden object-contain"
                       />
                     ) : (
