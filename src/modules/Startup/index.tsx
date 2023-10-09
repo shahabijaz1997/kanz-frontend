d233c26468dcc5643db7887fadff41e44a5bbed0
ahmadwb <ahmad.habib@whizzbridge.com>
2023-10-09 11:52:23 +0500

    Added loader on main listing page

--- src/modules/Startup/index.tsx
+++ src/modules/Startup/index.tsx
@@ -68,42 +68,46 @@ const Startup = ({ }: any) => {
             </section>
             <aside className="w-full h-full flex items-start justify-start">
                 <Sidebar type={KanzRoles.STARTUP} />
-                <div className="absolute left-0 top-0 w-full h-full grid place-items-center" style={{ backgroundColor: "rgba(0, 0, 0, 0.078)" }}>
-                    <Spinner />
-                </div>
-                <section className="bg-cbc-auth h-full p-[5rem]" style={{ width: "calc(100% - 250px)" }}>
-                    <section className="inline-flex justify-between items-center w-full">
-                        <h1 className="text-black font-medium text-2xl mb-2">{language?.v3?.startup?.overview?.heading_2}</h1>
-                    </section>
-
-                    <section className="mt-10 mb-16">
-                        <DealTable />
-                    </section>
-
-                    <section className="inline-flex justify-between items-center w-full">
-                        <div className="w-full">
-                            <h1 className="text-black font-medium text-2xl mb-2">{language?.v3?.startup?.overview?.heading}</h1>
-
-                            <span className="w-full flex items-center gap-5">
-                                <div className="rounded-md shadow-cs-6 bg-white border-[1px] border-gray-200 h-9 overflow-hidden max-w-[310px] inline-flex items-center px-2">
-                                    <SearchIcon />
-                                    <input type="search" className="h-full w-full outline-none pl-2 text-sm font-normal text-gray-400" placeholder={language?.v3?.common?.search} />
-                                </div>
-
-                                <ul className="inline-flex items-center">
-                                    {React.Children.toArray(tabs.map(tab => <li onClick={() => setSelectedTab(tab)} className={`py-2 px-3 font-medium cursor-pointer rounded-md transition-all ${selectedTab === tab ? "text-neutral-900 bg-neutral-100" : "text-gray-500"} `}>{tab} &nbsp;(0)</li>))}
-                                </ul>
-                            </span>
+                <section className="bg-cbc-auth h-full p-[5rem] relative" style={{ width: "calc(100% - 250px)" }}>
+                    {loading && (
+                        <div className="absolute left-0 top-0 w-full h-full grid place-items-center" style={{ backgroundColor: "rgba(0, 0, 0, 0.078)" }}>
+                            <Spinner />
                         </div>
-                        <Button onClick={() => setModalOpen(true)} className="w-[170px]">{language?.v3?.button?.new_deal}</Button>
-                    </section>
+                    )}
+                    <React.Fragment>
+                        <section className="inline-flex justify-between items-center w-full">
+                            <h1 className="text-black font-medium text-2xl mb-2">{language?.v3?.startup?.overview?.heading_2}</h1>
+                        </section>
 
-                    <section className="mt-10">
-                        <Table columns={columns} data={deals} onclick={(row: any) => {
-                            dispatch(saveDataHolder(row.id));
-                            navigate(`/create-deal/1`);
-                        }} noDataNode={<Button onClick={() => setModalOpen(true)} className="absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]">{language?.v3?.button?.new_deal}</Button>} />
-                    </section>
+                        <section className="mt-10 mb-16">
+                            <DealTable />
+                        </section>
+
+                        <section className="inline-flex justify-between items-center w-full">
+                            <div className="w-full">
+                                <h1 className="text-black font-medium text-2xl mb-2">{language?.v3?.startup?.overview?.heading}</h1>
+
+                                <span className="w-full flex items-center gap-5">
+                                    <div className="rounded-md shadow-cs-6 bg-white border-[1px] border-gray-200 h-9 overflow-hidden max-w-[310px] inline-flex items-center px-2">
+                                        <SearchIcon />
+                                        <input type="search" className="h-full w-full outline-none pl-2 text-sm font-normal text-gray-400" placeholder={language?.v3?.common?.search} />
+                                    </div>
+
+                                    <ul className="inline-flex items-center">
+                                        {React.Children.toArray(tabs.map(tab => <li onClick={() => setSelectedTab(tab)} className={`py-2 px-3 font-medium cursor-pointer rounded-md transition-all ${selectedTab === tab ? "text-neutral-900 bg-neutral-100" : "text-gray-500"} `}>{tab} &nbsp;(0)</li>))}
+                                    </ul>
+                                </span>
+                            </div>
+                            <Button onClick={() => setModalOpen(true)} className="w-[170px]">{language?.v3?.button?.new_deal}</Button>
+                        </section>
+
+                        <section className="mt-10">
+                            <Table columns={columns} data={deals} onclick={(row: any) => {
+                                dispatch(saveDataHolder(row.id));
+                                navigate(`/create-deal/1`);
+                            }} noDataNode={<Button onClick={() => setModalOpen(true)} className="absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]">{language?.v3?.button?.new_deal}</Button>} />
+                        </section>
+                    </React.Fragment>
                 </section>
             </aside>
 
