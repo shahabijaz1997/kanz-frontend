cb79b517a6ef8374d6a091152435f48e0bcdfa8a
ahmadwb <ahmad.habib@whizzbridge.com>
2023-10-11 15:33:44 +0500

    Fix in deal show

--- src/modules/CreateDeal/index.tsx
+++ src/modules/CreateDeal/index.tsx
@@ -327,9 +327,10 @@ const CreateDeal = () => {
         <section className="mb-8 w-full relative">
           <div className="relative rounded-md w-full h-10 border-[1px] border-neutral-300 bg-white overflow-hidden inline-flex items-center px-3">
             <input value={ques?.value} onInput={(e: any) => {
+              let regex = /[^0-9.]|(\.(?=.*\.))/g
               const enteredValue = e.target.value;
               const numericValue = enteredValue.replace(/[^0-9.]|(\.(?=.*\.))/g, "");
-              if (ques?.input_type === InputType.PERCENT && Number(e.target.value)) return;
+              if (ques?.input_type === InputType.PERCENT && Number(e.target.value) > 100) return;
 
               dispatch(saveDealSelection({ option: numericValue, question: ques, fields: dealData, lang: event, secIndex, step: dealData[step - 1] }))
             }} id={`num-${ques.id}`}
