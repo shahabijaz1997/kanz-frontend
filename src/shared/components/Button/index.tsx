9a2078692d0f960643669704f9a06a3e88d2fb9f
Momin Mahmud <momin.mahmud@whizzbridge.com>
2023-11-06 13:09:27 +0500

    Fixed Date fomatter, Fixed pagination, Fixed Invite button change on click

--- src/shared/components/Button/index.tsx
+++ src/shared/components/Button/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type ButtonProps = {
   type?: "primary" | "secondary" | "danger" | "outlined" | "ghost"; // Button type
   size?: "small" | "medium" | "large"; // Button size
-  onClick?: (t:any) => void; // Click event handler
+  onClick?: (t: any) => void; // Click event handler
   disabled?: boolean; // Disable the button
   loading?: boolean; // Show loading state
   htmlType?: "button" | "submit" | "reset"; // HTML button type
@@ -12,7 +12,7 @@ type ButtonProps = {
   className?: string; // Custom CSS className
   children?: React.ReactNode; // Suffix icon
   style?: React.CSSProperties; // button style
-  divStyle?:string; //
+  divStyle?: string; //
 };
 
 const Button: React.FC<ButtonProps> = ({
@@ -27,7 +27,7 @@ const Button: React.FC<ButtonProps> = ({
   className = "",
   children,
   style,
-  divStyle="flex items-center justify-center",
+  divStyle = "flex items-center justify-center",
   ...props
 }) => {
   // Map button type to Tailwind CSS className
@@ -68,13 +68,29 @@ const Button: React.FC<ButtonProps> = ({
         {...props}
         className={`
         inline-flex justify-center items-center px-4 py-2 text-sm font-semibold leading-6 transition duration-150 ease-in-out bg-cyan-800 rounded-md shadow hover:bg-cyan-900 
-        ${getTypeClass()} ${getSizeClass()} ${disabled || loading ? "opacity-50 cursor-not-allowed" : ""
-          } ${className}`}
+        ${getTypeClass()} ${getSizeClass()} ${
+          disabled || loading ? "opacity-50 cursor-not-allowed" : ""
+        } ${className}`}
       >
         {loading ? (
-          <svg className="w-5 h-5 mx-3 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" >
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          <svg
+            className="w-5 h-5 mx-3 -ml-1 text-white animate-spin"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             ></path>
           </svg>
         ) : (
