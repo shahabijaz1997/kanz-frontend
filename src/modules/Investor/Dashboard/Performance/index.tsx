137ee37b0759c3fa6a5c6bef7cc4d863e18da87e
Momin Mahmud <momin.mahmud@whizzbridge.com>
2023-12-01 23:16:16 +0500

    added investor Dashboard skeleton

---
+++ src/modules/Investor/Dashboard/Performance/index.tsx
@@ -0,0 +1,16 @@
+
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import SearchIcon from "../../../../ts-icons/searchIcon.svg";
+import React, { useEffect, useState } from "react";
+
+
+
+const Performance = ({}: any) :any => {
+
+    return( 
+        <h1>Performance</h1>
+    )
+
+};
+export default Performance;
