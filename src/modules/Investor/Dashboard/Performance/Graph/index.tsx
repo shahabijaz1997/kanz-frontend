29c5aca15be33eb9adcb9238b06203b284312350
Momin Mahmud <momin.mahmud@whizzbridge.com>
2024-01-23 16:55:41 +0500

    added loader to graph

--- src/modules/Investor/Dashboard/Performance/Graph/index.tsx
+++ src/modules/Investor/Dashboard/Performance/Graph/index.tsx
@@ -2,8 +2,9 @@ import { useSelector } from "react-redux";
 import { getInvestorAnalyticsFundingRoundInvestments, getInvestorAnalyticsInvestments, getInvestorAnalyticsInvestmentsChart, getInvestorAnalyticsPropertyInvestments } from "../../../../../apis/investor.api";
 import PerfomanceGraph from "../../../PerfomanceGraph";
 import { RootState } from "../../../../../redux-toolkit/store/store";
-import { useEffect, useState } from "react";
-const Graph = ({}: any) :any => {
+import React, { useEffect, useState } from "react";
+import Spinner from "../../../../../shared/components/Spinner";
+const Graph = ({}: any) :React.ReactElement => {
   const [loading, setLoading] = useState(false);
   const [graphData, setGraphData] = useState(null);
   const authToken: any = useSelector((state: RootState) => state.auth.value);
@@ -23,12 +24,23 @@ const Graph = ({}: any) :any => {
         console.log(error);
       }
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
   };
    
   console.log(graphData)
 
-    return <PerfomanceGraph data={graphData} />
+    
+  return loading ? (
+      <div
+        className="relative w-full h-full left-0 top-0 grid place-items-center"
+        style={{ backgroundColor: "rgba(255, 255, 255, 1)", zIndex: 50 }}
+      >
+        <Spinner />
+      </div>
+    ) : ( 
+    <PerfomanceGraph data={graphData} />
+    )
+    
 };
 export default Graph;
