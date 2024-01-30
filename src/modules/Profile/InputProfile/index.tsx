c9dbb17c5bc433568aaa3ceaaf251df115e73b3e
Momin Mahmud <momin.mahmud@whizzbridge.com>
2024-01-30 14:23:34 +0500

    fixed issues on profile management

--- src/modules/Profile/InputProfile/index.tsx
+++ src/modules/Profile/InputProfile/index.tsx
@@ -1,19 +1,19 @@
-const InputProfile = (props:any) => {
-    return (
-        <span className={` w-[60%] flex-col flex`}>
-        <p className="text-xs mb-1 font-medium whitespace-nowrap">
-          {props?.label}
-        </p>
-        <input
+const InputProfile = (props: any) => {
+  return (
+    <span className={` w-[60%] flex-col flex`}>
+      <p className=" mb-1 font-medium whitespace-nowrap">{props?.label}</p>
+      <input
+        placeholder={props?.placeholder}
         value={props?.value}
-          disabled={props?.disabled}
-          className={`text-[10px] px-2 py-1.5 w-full border-[1px] focus:border-[#155E75] rounded-md ${props?.disabled ? "text-gray-400 cursor-not-allowed " : "bg-white"}`}
-          type="text"
-          onChange={(e)=>props?.onChange(e.target.value)}
-        />
-      </span>
-    )
-    
-}
+        disabled={props?.disabled}
+        className={`text-sm px-2 py-1.5 w-full border-[1px] focus:border-[#155E75] rounded-md ${
+          props?.disabled ? "text-gray-400 cursor-not-allowed " : "bg-white"
+        }`}
+        type="text"
+        onChange={(e) => props?.onChange(e.target.value)}
+      />
+    </span>
+  );
+};
 
-export default InputProfile
+export default InputProfile;
