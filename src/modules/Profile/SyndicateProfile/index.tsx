c55568fa200118a4d41d7891ad4a38630fc96de6
NomanShahidWB <noman.shahid@whizzbridge.com>
2024-02-06 14:14:36 +0500

    Fixes in disabled

--- src/modules/Profile/SyndicateProfile/index.tsx
+++ src/modules/Profile/SyndicateProfile/index.tsx
@@ -232,7 +232,7 @@ const SyndciateProfile = ({
                 }}
                 onClick={() => {
                   setShowData(!showData);
-                  setShowFlex(true);
+                  setShowFlex(!showFlex);
                 }}
                 className=' text-sm px-2 py-1.5 w-full border-[1px] focus:border-[#155E75] rounded-md bg-white'
                 type='text'
@@ -423,7 +423,7 @@ const SyndciateProfile = ({
               onChange={setYouRaised}
             />
             <InputProfile
-              disabled={true}
+              disabled={false}
               label={'How many times you have raised?'}
               value={timesRaised}
               onChange={setTimesRaised}
